f2dd6a123d1e83cd383b22d5e66cc848badfcca2
Rajesh chaudhari <rajeshkumar76249@gmail.com>
2021-04-02 14:45:06 +0530

    show Data based on role in LeaveManagement

--- client/src/app/services/dashbaord.service.ts
+++ client/src/app/services/dashbaord.service.ts
@@ -1,14 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DashbaordService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getEmployeeInfoByEmpCode(empCode){
-    return this.http.get("http://localhost:8443/api/v1/employeeInfo/findByEmpCode/"+empCode);
+  getEmployeeInfoByEmpCode(empCode) {
+    return this.http.get(
+      "http://localhost:8443/api/v1/employeeInfo/findById/" + empCode
+    );
   }
 }
